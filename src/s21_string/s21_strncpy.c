45de8275acc422cc17b68b9eff900db8029cc03a
Hanzo Lemon <hanzolem@vo-h3.msk.21-school.ru>
2022-05-02 20:11:00 +0300

    start sprintf

--- src/s21_string/s21_strncpy.c
+++ src/s21_string/s21_strncpy.c
@@ -3,7 +3,10 @@
 // 12. Copies up to n characters from the string pointed to, by src to dest.
 char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
     char *out = dest;
-    while (n > 0 && *src != 0) {
+    int flag = 0;
+
+    while (n > 0 && !flag) {
+        flag = !src;
         *dest = *src;
         dest++;
         src++;
