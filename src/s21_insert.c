0dbd52454b8e8657e3fc4b27e928f55cf67217b7
Shena Tammara <shenatam@vo-f3.msk.21-school.ru>
2022-05-25 19:47:31 +0300

    fix

--- src/s21_insert.c
+++ src/s21_insert.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
-
 #include "s21_string.h"
 
 void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
-    s21_size_t length = s21_strlen(str) + s21_strlen(src);
-    char *out = (char *)malloc(length + 1);
+    char *out = S21_NULL;
+    s21_size_t length_str = (str) ? s21_strlen(str) : 0;
+    s21_size_t length = 0;
+    if (src && (start_index <= s21_strlen(src))) {
+        length = length_str + s21_strlen(src);
+        out = (char *)malloc(length + 1);
+    }
     if (out != S21_NULL) {
         for (s21_size_t i = 0; i < length; i++) {
             // Вставка строки str
-            if (i >= start_index - 1 && *str) {
+            if (i >= start_index && *str) {
                 out[i] = *str;
                 str++;
                 // Вставка основной строки src
--- src/s21_trim.c
+++ src/s21_trim.c
@@ -1,7 +1,15 @@
 #include <stdlib.h>
 #include "s21_string.h"
 void *s21_trim(const char *src, const char *trim_chars) {
-    s21_size_t length_src = s21_strlen(src);
+    char* space = " ";
+    const char* chars = space;
+    if (trim_chars) {
+        if (*trim_chars) {
+            chars = trim_chars;
+        } 
+    }
+    s21_size_t length_src = 0;
+    if (src) length_src = s21_strlen(src);
     char* reverse = (char*)malloc(sizeof(char) * (length_src + 1));
     // Переворот строки
     for (s21_size_t i = 0; i < length_src; i++) {
@@ -10,11 +18,11 @@ void *s21_trim(const char *src, const char *trim_chars) {
     reverse[length_src] = '\0';
     s21_size_t begin_segment = 0;
     s21_size_t end_segment = 0;
-    if (trim_chars) {
+    if (src) {
         // Длина начального сегмента, состоящего из заданных символов
-        begin_segment = s21_strspn(src, trim_chars);
+        begin_segment = s21_strspn(src, chars);
         // Длина конечного сегмента, состоящего из заданных символов
-        end_segment = s21_strspn(reverse, trim_chars);
+        end_segment = s21_strspn(reverse, chars);
     }
     s21_size_t length_out = 1;
     if (begin_segment != length_src) {
