1f6507356e6eca5b728eda3b50e1a035a40954ac
Hanzo Lemon <hanzolem@il-f3.msk.21-school.ru>
2022-06-25 15:57:24 +0300

    hueta2

--- src/memset.c
+++
@@ -1,9 +0,0 @@
-#include "s21_string.h"
-
-void *s21_memset(void *str, int c, s21_size_t n) {
-    unsigned char *ptr = str;
-    while (n--) {
-        *ptr++ = (unsigned char)c;
-    }
-    return str;
-}
